ff57bf796beee556500d342c6012a4050a67a236
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-09 15:54:58 +0100

    Initial commit for LoansMicroserviceApplication

---
+++ Loans_Microservice/src/main/java/com/example/loans_microservice/LoansMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.loans_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Loans_Microservice/src/test/java/com/example/loans_microservice/LoansMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loans_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
