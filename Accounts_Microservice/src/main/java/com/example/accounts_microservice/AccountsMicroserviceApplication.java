bc2568ec2c9be103314bdeb053f420d40d84835d
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-09 15:52:09 +0100

    Initial commit

---
+++ Accounts_Microservice/src/main/java/com/example/accounts_microservice/AccountsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.accounts_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Accounts_Microservice/src/test/java/com/example/accounts_microservice/AccountsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accounts_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
