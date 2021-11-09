1d5a82439fecbf47ff2ee9ecf4bfc9cb885a6796
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-09 15:59:27 +0100

    Initial commit for CardsMicroserviceApplication

---
+++ Cards_Microservice/src/main/java/com/mrdiipo/cards_microservice/CardsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.cards_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Cards_Microservice/src/test/java/com/mrdiipo/cards_microservice/CardsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.cards_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
