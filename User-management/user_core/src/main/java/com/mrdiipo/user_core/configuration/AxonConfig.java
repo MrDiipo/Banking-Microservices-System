ebdcb581d6ff79f752ad0dba48cbe4249b2834ac
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-15 11:53:21 +0100

    AxonConfig implementation

---
+++ User-management/user_core/src/main/java/com/mrdiipo/user_core/configuration/AxonConfig.java
@@ -0,0 +1,24 @@
+package com.mrdiipo.user_core.configuration;
+
+import com.mongodb.client.MongoClient;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+    @Value("${spring.data.mongodb.host:127.0.0.1}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port:27017}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.database:user}")
+    private  String mongoDatabase;
+
+}
