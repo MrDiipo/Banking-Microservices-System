584dad0adeac41e2837ffc90974b92ebef4155f5
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-16 10:47:05 +0100

    Spring security configuration

--- User-management/user_core/src/main/java/com/mrdiipo/user_core/models/Account.java
+++ User-management/user_core/src/main/java/com/mrdiipo/user_core/models/Account.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,8 +15,13 @@ import java.util.List;
 @Builder
 public class Account {
 
+    @Size(min = 5, message = "Username must have a minimum of 5 characters")
     private String username;
+
+    @Size(min = 8, message = "Password must have a minimum of 8 characters")
     private String password;
+
+    @NotNull(message = "Specify at least one role")
     private List<Role> roles;
 
 }
--- User-management/user_core/src/main/java/com/mrdiipo/user_core/models/User.java
+++ User-management/user_core/src/main/java/com/mrdiipo/user_core/models/User.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,9 +20,13 @@ public class User {
     @Id
     private String id;
 
+    @NotEmpty(message = "Firstname is mandatory")
     private String firstName;
+    @NotEmpty(message = "Lastname is mandatory")
     private String lastName;
 
+    @Email(message = "Please provice a valid email address")
     private String emailAddress;
+    @NotNull(message = "Please provide account credentials")
     private Account account;
 }
