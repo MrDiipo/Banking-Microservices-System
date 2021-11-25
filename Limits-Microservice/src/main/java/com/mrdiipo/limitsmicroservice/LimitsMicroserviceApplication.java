303503c6f7848b53a77618693a02eed1066a2534
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-25 10:40:33 +0100

    Code updates

---
+++ Limits-Microservice/src/main/java/com/mrdiipo/limitsmicroservice/LimitsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.limitsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitsMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Limits-Microservice/src/test/java/com/mrdiipo/limitsmicroservice/LimitsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.limitsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
