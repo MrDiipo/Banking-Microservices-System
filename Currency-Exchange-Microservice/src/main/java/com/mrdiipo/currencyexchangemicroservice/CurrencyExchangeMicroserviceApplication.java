f1a7e2e6e778140a2c6e092e6f9ad14532fb38f0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-27 00:15:41 +0100

    Add Currency-Exchange-Microservice 2

---
+++ Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/CurrencyExchangeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.currencyexchangemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeMicroserviceApplication.class, args);
+    }
+
+}
