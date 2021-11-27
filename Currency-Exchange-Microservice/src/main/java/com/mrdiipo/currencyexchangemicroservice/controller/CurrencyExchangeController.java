5c6917f13980eda2fd378e2fb91df8b12bbc5de0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-27 18:27:29 +0100

    Implementing Currency Convertion.

--- Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/controller/CurrencyExchangeController.java
+++ Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/controller/CurrencyExchangeController.java
@@ -1,16 +1,65 @@
 package com.mrdiipo.currencyexchangemicroservice.controller;
 
-import com.mrdiipo.currencyexchangemicroservice.model.ExchangeValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mrdiipo.currencyexchangemicroservice.controller.request.CurrencyRequestModel;
+import com.mrdiipo.currencyexchangemicroservice.controller.response.CurrencyResponseModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 public class CurrencyExchangeController {
 
-    @GetMapping("/api/v1/{from}/to/{to}")
-    public ExchangeValue getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to){
+    String API_KEY = "https://v6.exchangerate-api.com/v6/a0400acdd22ea970ab5a5a9c/pair/";
 
-        return null;
+
+    @PostMapping("/api/v1/getExchangeValue")
+    public CurrencyResponseModel getExchangeValue(@RequestBody CurrencyRequestModel currencyRequestModel) throws IOException {
+
+        String fromCurrency = currencyRequestModel.getFrom();
+        String toCurrency = currencyRequestModel.getTo();
+        BigDecimal amount =  currencyRequestModel.getValue();
+
+        String requestString = API_KEY + fromCurrency + "/" + toCurrency + "/" + amount.toString();
+
+        URL url = new URL(requestString);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        String result = jsonobj.get("conversion_result").getAsString();
+        return CurrencyResponseModel.builder()
+                .value(
+                        BigDecimal.valueOf(Double.parseDouble(result)))
+                .build();
+
+
+
+//        CurrencyRequestModel requestModel = CurrencyRequestModel.builder().value(currencyRequestModel.getValue())
+//                .from(currencyRequestModel.getFrom())
+//                .to(currencyRequestModel.getTo())
+//                .build();
+//
+//        MonetaryAmount convertCurrency = Monetary.getDefaultAmountFactory()
+//                .setCurrency(requestModel.getFrom())
+//                .setNumber(requestModel.getValue())
+//                .create();
+//
+//        CurrencyConversion toCurrency = MonetaryConversions.getConversion(requestModel.getTo());
+//
+//        MonetaryAmount  convertedValue = convertCurrency.with(toCurrency);
+
     }
 }
---
+++ Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/controller/response/CurrencyResponseModel.java
@@ -0,0 +1,12 @@
+package com.mrdiipo.currencyexchangemicroservice.controller.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.money.MonetaryAmount;
+import java.math.BigDecimal;
+@Data
+@Builder
+public class CurrencyResponseModel {
+    private BigDecimal value;
+}
