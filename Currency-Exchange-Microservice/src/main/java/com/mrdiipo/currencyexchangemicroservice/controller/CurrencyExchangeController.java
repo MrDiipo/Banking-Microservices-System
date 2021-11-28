a2fae705b16be2d39442396df2d9d5cc61103adf
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-28 19:48:16 +0100

    Add Ribbon Client Dependency

--- Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/controller/CurrencyExchangeController.java
+++ Currency-Exchange-Microservice/src/main/java/com/mrdiipo/currencyexchangemicroservice/controller/CurrencyExchangeController.java
@@ -4,6 +4,7 @@ import com.mrdiipo.currencyexchangemicroservice.controller.request.CurrencyReque
 import com.mrdiipo.currencyexchangemicroservice.controller.response.CurrencyResponseModel;
 import com.mrdiipo.currencyexchangemicroservice.service.CurrencyExchangeServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.math.BigDecimal;
 
+@RibbonClient("accounts-query-service")
 @RestController
 public class CurrencyExchangeController {
 
     @Autowired CurrencyExchangeServiceImp currencyExchangeServiceImp;
 
-    @PostMapping("/api/v1/getExchangeValue")
+    @PostMapping("/api/v1/getConvertedValue")
     public CurrencyResponseModel getExchangeValue(@RequestBody CurrencyRequestModel currencyRequestModel) throws IOException {
 
         CurrencyRequestModel requestModel = CurrencyRequestModel.builder().amount(currencyRequestModel.getAmount())
