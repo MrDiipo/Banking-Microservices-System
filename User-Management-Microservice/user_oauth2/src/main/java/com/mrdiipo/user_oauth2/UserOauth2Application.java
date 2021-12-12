3e42e269ae3432dfb28256cecb726cc7d10caca7
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-12-12 15:51:49 +0100

    Configure Web Security

--- User-Management-Microservice/user_oauth2/src/main/java/com/mrdiipo/user_oauth2/UserOauth2Application.java
+++ User-Management-Microservice/user_oauth2/src/main/java/com/mrdiipo/user_oauth2/UserOauth2Application.java
@@ -2,8 +2,10 @@ package com.mrdiipo.user_oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class UserOauth2Application {
 
     public static void main(String[] args) {
