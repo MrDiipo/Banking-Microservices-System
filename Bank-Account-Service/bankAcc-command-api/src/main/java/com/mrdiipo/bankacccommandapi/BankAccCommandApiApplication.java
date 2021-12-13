5421ec42c3de2fe6760a474381876a0986da3675
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-12-13 13:40:21 +0100

    Update property files

--- Bank-Account-Service/bankAcc-command-api/src/main/java/com/mrdiipo/bankacccommandapi/BankAccCommandApiApplication.java
+++ Bank-Account-Service/bankAcc-command-api/src/main/java/com/mrdiipo/bankacccommandapi/BankAccCommandApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class})
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BankAccCommandApiApplication {
