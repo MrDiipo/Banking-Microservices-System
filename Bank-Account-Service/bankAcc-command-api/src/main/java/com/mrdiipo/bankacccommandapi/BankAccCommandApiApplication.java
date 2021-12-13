c74835046aed4645c298f0c1e2256165c9a230cf
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-12-13 13:33:11 +0100

    Include minor configs

--- Bank-Account-Service/bankAcc-command-api/src/main/java/com/mrdiipo/bankacccommandapi/BankAccCommandApiApplication.java
+++ Bank-Account-Service/bankAcc-command-api/src/main/java/com/mrdiipo/bankacccommandapi/BankAccCommandApiApplication.java
@@ -2,8 +2,13 @@ package com.mrdiipo.bankacccommandapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BankAccCommandApiApplication {
 
     public static void main(String[] args) {
--- Bank-Account-Service/bankAcc-query-api/src/main/java/com/mrdiipo/bankaccqueryapi/BankAccQueryApiApplication.java
+++ Bank-Account-Service/bankAcc-query-api/src/main/java/com/mrdiipo/bankaccqueryapi/BankAccQueryApiApplication.java
@@ -2,8 +2,12 @@ package com.mrdiipo.bankaccqueryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BankAccQueryApiApplication {
 
     public static void main(String[] args) {
